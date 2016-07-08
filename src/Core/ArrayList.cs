af1d76308ab9d160fd608bc6e8fbdf045e549a84
Atif Aziz <code@raboof.com>
2016-07-08 18:03:46 +0200

    Get rows given a table with column and row span handling

---
+++ src/Core/ArrayList.cs
@@ -0,0 +1,74 @@
+#region Copyright (c) 2016 Atif Aziz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace WebLinq
+{
+    using System;
+    using System.Linq;
+
+    struct ArrayList<T>
+    {
+        static readonly T[] Zero = new T[0];
+
+        T[] _items;
+
+        public int Count { get; private set; }
+
+        public void EnsureCapacity(int capacity)
+        {
+            if (capacity <= (Capacity ?? 0))
+                return;
+            Array.Resize(ref _items, TwoPowers.RoundUpToClosest(capacity));
+        }
+
+        public T this[int index]
+        {
+            get { return _items[index]; }
+            set
+            {
+                EnsureCapacity(index + 1);
+                _items[index] = value;
+                Count = Math.Max(Count, index + 1);
+            }
+        }
+
+        public T this[int index, T defaultValue] =>
+            index < Count ? this[index] : defaultValue;
+
+        int? Capacity => _items?.Length;
+
+        public T[] ToArray()
+        {
+            if (Count == 0)
+                return Zero;
+            Array.Resize(ref _items, Count);
+            return _items;
+        }
+    }
+
+    static class TwoPowers
+    {
+        static readonly int[] Cache = Enumerable.Range(0, 31).Select(p => 1 << p).ToArray();
+
+        public static int RoundUpToClosest(int x)
+        {
+            if (x < 0 || x > Cache[Cache.Length - 1])
+                throw new ArgumentOutOfRangeException(nameof(x), x, null);
+            var i = Array.BinarySearch(Cache, x);
+            return Cache[i >= 0 ? i : ~i];
+        }
+    }
+}
--- src/Core/Html/ParsedHtml.cs
+++ src/Core/Html/ParsedHtml.cs
@@ -19,6 +19,7 @@ namespace WebLinq.Html
     using System;
     using System.Collections.Generic;
     using System.Collections.Specialized;
+    using System.Globalization;
     using System.Linq;
     using System.Net;
     using System.Net.Mime;
@@ -236,6 +237,86 @@ namespace WebLinq.Html
                  : selector2 != null ? selector2(form, submittables)
                  : selector1 != null ? selector1(form)
                  : default(T);
+        }
+
+        public static IEnumerable<T> TableRows<T>(this HtmlObject table, Func<HtmlObject, IEnumerable<HtmlObject>, T> rowSelector)
+        {
+            if (table == null) throw new ArgumentNullException(nameof(table));
+            if (!table.IsNamed("table"))
+                throw new ArgumentException($"Element is <{table.Name}> and not an HTML table.", nameof(table));
+            if (rowSelector == null) throw new ArgumentNullException(nameof(rowSelector));
+            return TableRowsCore(table, rowSelector);
+        }
+
+        static IEnumerable<T> TableRowsCore<T>(HtmlObject table, Func<HtmlObject, IEnumerable<HtmlObject>, T> rowSelector)
+        {
+            // TODO https://www.w3.org/TR/html5/tabular-data.html#processing-model-1
+
+            var spans = new ArrayList<CellSpan>();
+
+            foreach (var e in
+                from g in table.ChildElements
+                where g.IsNamed("tbody") || g.IsNamed("thead") || g.IsNamed("tfoot") || g.IsNamed("tr")
+                from tr in g.IsNamed("tr")
+                         ? new[] { g } :
+                         g.ChildElements.Where(e => e.IsNamed("tr"))
+                select new
+                {
+                    Row   = tr,
+                    Cells = tr.ChildElements.Where(e => e.IsNamed("td") || e.IsNamed("th")).ToArray(),
+                })
+            {
+                var tds = e.Cells;
+                spans.EnsureCapacity(tds.Length);
+
+                var cells = new ArrayList<HtmlObject>();
+                cells.EnsureCapacity(tds.Length);
+
+                var i = 0;
+                foreach (var td in tds)
+                {
+                    while (true)
+                    {
+                        var rspan = spans[i, CellSpan.Zero];
+                        if (rspan.Rows == 0)
+                            break;
+                        i += rspan.Cols;
+                    }
+
+                    var colspan = TryParse.Int32(td.GetAttributeValue("colspan"), NumberStyles.Integer & ~NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture) ?? 1;
+                    var rowspan = TryParse.Int32(td.GetAttributeValue("rowspan"), NumberStyles.Integer & ~NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture) ?? 1;
+                    var span = colspan > 1 || rowspan > 1 ? new CellSpan(colspan, rowspan) : CellSpan.One;
+
+                    spans[i] = span;
+                    cells[i] = td;
+
+                    i += span.Cols;
+                }
+
+                for (i = 0; i < spans.Count; i++)
+                {
+                    var span = spans[i];
+                    var rowspan = span.Rows - 1;
+                    spans[i] = rowspan > 0 ? new CellSpan(span.Cols, rowspan) : CellSpan.Zero;
+                }
+
+                yield return rowSelector(e.Row, cells.ToArray());
+            }
+        }
+
+        struct CellSpan : IEquatable<CellSpan>
+        {
+            public static readonly CellSpan Zero = new CellSpan(0, 0);
+            public static readonly CellSpan One = new CellSpan(1, 1);
+
+            public readonly int Cols;
+            public readonly int Rows;
+
+            public CellSpan(int cols, int rows) { Rows = rows; Cols = cols; }
+            public bool Equals(CellSpan other) => Cols == other.Cols && Rows == other.Rows;
+            public override bool Equals(object obj) => obj is CellSpan && Equals((CellSpan)obj);
+            public override int GetHashCode() => unchecked((Cols * 397) ^ Rows);
+            public override string ToString() => $"[{Cols}, {Rows}]";
         }
     }
 }
