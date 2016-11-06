5144eb9037460e8a8afbf72fcefd8331005708be
Atif Aziz <code@raboof.com>
2016-11-06 19:38:10 +0100

    Remove poor example.com sample
    There are much richer samples and the initial one based on example.com was somewhat superficial.

--- eg/Program.cs
+++ eg/Program.cs
@@ -23,38 +23,9 @@ namespace WebLinq.Samples
     {
         public static void Main()
         {
-            HttpGetWithLinksAndHtmlParsing();
             GoogleSearch();
-            ScheduledTasksViaSpawn();
             QueenSongs();
-        }
-
-        static void HttpGetWithLinksAndHtmlParsing()
-        {
-            var q =
-                from com in Http.UserAgent(@"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-                                .Get(new Uri("http://www.example.com/"))
-                                .Html()
-                select new { com.Id, Html = com.Content.QuerySelector("p")?.OuterHtml }
-                into com
-                from net in Http.Get(new Uri("http://www.example.net/"))
-                                .Html()
-                from link in Links(net.Content)
-                select new
-                {
-                    Com = com,
-                    Net = new
-                    {
-                        net.Id,
-                        Html = net.Content.QuerySelector("p")?.OuterHtml,
-                        Link = link,
-                    }
-                }
-                into e
-                where e.Com.Html?.Length == e.Net.Html?.Length
-                select e;
-
-            q.Dump();
+            ScheduledTasksViaSpawn();
         }
 
         static void GoogleSearch()
