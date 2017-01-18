a0377f14d016f407c53673ae468f93371e59995b
Atif Aziz <code@raboof.com>
2017-01-18 17:55:53 +0100

    CSV output

---
+++ src/Core/Output.cs
@@ -0,0 +1,140 @@
+#region Copyright (c) 2016 Atif Aziz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace WebLinq
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Reactive.Linq;
+    using System.Reflection;
+    using Mannex.Collections.Generic;
+
+    public sealed class CsvOutputFormatBuilder<T>
+    {
+        readonly IObservable<T> _query;
+        List<KeyValuePair<string, Func<T, object>>> _fields;
+
+        internal CsvOutputFormatBuilder(IObservable<T> query)
+        {
+            if (query == null) throw new ArgumentNullException(nameof(query));
+            _query = query;
+        }
+
+        bool HasFields => _fields?.Count > 0;
+
+        ICollection<KeyValuePair<string, Func<T, object>>> Fields =>
+            _fields ?? (_fields = new List<KeyValuePair<string, Func<T, object>>>());
+
+        public CsvOutputFormatBuilder<T> Field(string name, Func<T, object> selector)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (selector == null) throw new ArgumentNullException(nameof(selector));
+            Fields.Add(name.AsKeyTo(selector));
+            return this;
+        }
+
+        static readonly IEnumerable<KeyValuePair<string, Func<T, object>>>
+            SingletonField =
+                new[] { "Item".AsKeyTo(new Func<T, object>(e => e)) };
+
+        public CsvOutputFormatBuilder<T> Reflect() => Reflect(s => s);
+
+        public CsvOutputFormatBuilder<T> Reflect(Func<string, string> nameMapper)
+        {
+            if (nameMapper == null) throw new ArgumentNullException(nameof(nameMapper));
+
+            var type = typeof(T);
+
+            var fields =
+                Type.GetTypeCode(type) != TypeCode.Object
+                    ? SingletonField
+                    : from props in new[] { type.GetProperties(BindingFlags.Public | BindingFlags.Instance) }
+                      from e in
+                          props.Length == 0
+                          ? SingletonField
+                          : from p in props
+                            where p.CanRead
+                            select p.Name.AsKeyTo(new Func<T, object>(e => p.GetValue(e)))
+                      select e;
+
+            return fields.Aggregate(this, (b, e) => b.Field(nameMapper(e.Key), e.Value));
+        }
+
+        public IObservable<string> Lines(bool includeHeaders = false) =>
+            !HasFields
+            ? Reflect().Lines(includeHeaders)
+            : _query.Select(e => string.Join(",", Fields.Select(f => Csv.EncodeField(f.Value(e)))))
+                    .StartWith(Enumerable.Repeat(string.Join(",", Fields.Select(f => Csv.EncodeField(f.Key))), 1))
+                    .Skip(includeHeaders ? 0 : 1);
+    }
+
+    public static class CsvOutputFormat
+    {
+        public static IObservable<string> ToCsv<T>(this IObservable<T> query) =>
+            new CsvOutputFormatBuilder<T>(query).Lines();
+
+        public static IObservable<ICsvOutputFormatPartial> Partial =
+            Observable.Return(CsvOutputFormatPartial.Singleton);
+    }
+
+    public interface ICsvOutputFormatPartial
+    {
+        CsvOutputFormatBuilder<T> Source<T>(IObservable<T> query);
+    }
+
+    sealed class CsvOutputFormatPartial : ICsvOutputFormatPartial
+    {
+        internal static readonly ICsvOutputFormatPartial Singleton = new CsvOutputFormatPartial();
+
+        public CsvOutputFormatBuilder<T> Source<T>(IObservable<T> query) => new CsvOutputFormatBuilder<T>(query);
+
+        CsvOutputFormatPartial() { }
+    }
+
+    static class Csv
+    {
+        static readonly char[] UnquotedCsvFieldProhibitedChars = { ',', '"', '\r', '\n' };
+
+        internal static string EncodeField<T>(T value)
+        {
+            const string quote = "\"";
+            const string quotequote = quote + quote;
+            var v = string.Format(CultureInfo.InvariantCulture, "{0}", value);
+            return v.IndexOfAny(UnquotedCsvFieldProhibitedChars) >= 0
+                 ? quote + v.Replace(quote, quotequote) + quote
+                 : v;
+        }
+    }
+}
+
+namespace WebLinq.NamingConventions
+{
+    using System.Text.RegularExpressions;
+
+    public static class SnakeCase
+    {
+        static string FromPascalCore(string input) =>
+            Regex.Replace(input, @"((?<![A-Z]|^)[A-Z]|(?<=[A-Z]+)[A-Z](?=[a-z]))", m => "_" + m.Value);
+
+        public static string FromPascal(string input) =>
+            FromPascalCore(input).ToLowerInvariant();
+
+        public static string ScreamingFromPascal(string input) =>
+            FromPascalCore(input).ToUpperInvariant();
+    }
+}
