f636318ba449bccad14a2fa98508ed981b364460
Atif Aziz <code@raboof.com>
2018-08-10 11:36:45 +0200

    Fix packaging to include dependencies & metadata

--- pack.cmd
+++ pack.cmd
@@ -10,11 +10,13 @@ if not exist dist md dist
 if not %errorlevel%==0 exit /b %errorlevel%
 set REPO_COMMIT=N/A
 if not "%~2"=="" set REPO_COMMIT=%2
-set VERSION_SUFFIX=
-if not "%~1"=="" set VERSION_SUFFIX=-%1
-call build /v:m ^
-  && NuGet pack src\Core\WebLinq.csproj                      ^
-                -Symbol -OutputDirectory dist                ^
-                -Properties "VersionSuffix=%VERSION_SUFFIX%" ^
-                -Properties "RepoCommit=%REPO_COMMIT%"
+for /f "usebackq tokens=*" %%v in (`PowerShell -C "type src\Core\WebLinq.csproj | ? { $_ -match '(?<=<PackageVersion>)[0-9]+(\.[0-9]+){2}' } | %% { $Matches[0] }"`) do (
+    set VERSION=%%v
+)
+if not "%~1"=="" set VERSION=%VERSION%-%1
+   call build ^
+&& call msbuild  /t:Pack src\Core\WebLinq.csproj /v:m   ^
+                "/p:Configuration=Release"              ^
+                "/p:PackageVersion=%VERSION%"           ^
+                "/p:PackageReleaseNotes=Commit @ %REPO_COMMIT%"
 goto :EOF
