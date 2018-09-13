b1d9c89aba4d51e9e02fd9d18540be7abed3f7f5
Atif Aziz <code@raboof.com>
2018-09-13 16:15:36 +0200

    Bring back code coverage

--- test.cmd
+++ test.cmd
@@ -6,10 +6,12 @@ goto :EOF
 
 :main
     call build ^
- && call :test Debug ^
+ && call :test Debug -p:CollectCoverage=true ^
+                     -p:CoverletOutputFormat=opencover ^
+                     -p:Exclude=[NUnit*]* ^
  && call :test Release
 goto :EOF
 
 :test
-dotnet test --no-build tests -c %1
+dotnet test --no-build tests -c %*
 goto :EOF
