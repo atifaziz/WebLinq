9cf890e5e925088e2871dc7bdce4478346ad4310
Atif Aziz <code@raboof.com>
2018-09-19 16:55:19 +0200

    Revert "Break testing on Windows to test CI"
    This reverts commit 687e81e50bf23c1371c10e4dbecf435c00cc751a.

--- test.cmd
+++ test.cmd
@@ -1,1 +1,17 @@
-exit /b 42
+@echo off
+pushd "%~dp0"
+call :main %*
+popd
+goto :EOF
+
+:main
+    call build ^
+ && call :test Debug -p:CollectCoverage=true ^
+                     -p:CoverletOutputFormat=opencover ^
+                     -p:Exclude=[NUnit*]* ^
+ && call :test Release
+goto :EOF
+
+:test
+dotnet test --no-build tests -c %*
+goto :EOF
