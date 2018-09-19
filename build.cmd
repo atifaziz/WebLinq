6c04ba47665d9b3d37c239b82decc62c4c1ae891
Atif Aziz <code@raboof.com>
2018-09-19 16:17:20 +0200

    Break building on Windows to test CI

--- build.cmd
+++ build.cmd
@@ -1,12 +1,1 @@
-@echo off
-pushd "%~dp0"
-call :main %*
-popd
-goto :EOF
-
-:main
-    dotnet restore ^
- && dotnet --info ^
- && dotnet build --no-restore -c Debug ^
- && dotnet build --no-restore -c Release
-goto :EOF
+exit /b 42
