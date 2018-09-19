687e81e50bf23c1371c10e4dbecf435c00cc751a
Atif Aziz <code@raboof.com>
2018-09-19 16:47:03 +0200

    Break testing on Windows to test CI

--- test.cmd
+++ test.cmd
@@ -1,17 +1,1 @@
-@echo off
-pushd "%~dp0"
-call :main %*
-popd
-goto :EOF
-
-:main
-    call build ^
- && call :test Debug -p:CollectCoverage=true ^
-                     -p:CoverletOutputFormat=opencover ^
-                     -p:Exclude=[NUnit*]* ^
- && call :test Release
-goto :EOF
-
-:test
-dotnet test --no-build tests -c %*
-goto :EOF
+exit /b 42
