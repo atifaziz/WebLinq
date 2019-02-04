31990855c6bb8f29e959f1433402ac58fe0fc6f8
Atif Aziz <code@raboof.com>
2019-02-04 18:29:03 +0100

    Remove WithTimeout for now (YAGNI?)

--- src/Core/HttpQuery.cs
+++ src/Core/HttpQuery.cs
@@ -234,10 +234,6 @@ namespace WebLinq
                     SendAsync(http, ho.Configurer(http.Config), 0, HttpMethod.Post, url, content, ho.Options)
                         .ToObservable()));
 
-        public static IObservable<HttpFetch<T>> WithTimeout<T>(this IObservable<HttpFetch<T>> query, TimeSpan duration) =>
-            from e in query
-            select e.WithConfig(e.Client.Config.WithTimeout(duration));
-
         public static IObservable<HttpFetch<T>> WithUserAgent<T>(this IObservable<HttpFetch<T>> query, string ua) =>
             from e in query
             select e.WithConfig(e.Client.Config.WithUserAgent(ua));
